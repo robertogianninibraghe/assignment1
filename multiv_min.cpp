78faa47350fc147f757a388bcf570fd2e9039f0d
robertogianninibraghe <roberto.giannini@mail.polimi.it>
2024-03-23 21:14:51 +0100

    More explainative readfile and more comments on multiv_min.cpp

--- multiv_min.cpp
+++ multiv_min.cpp
@@ -1,11 +1,8 @@
 #include "multiv_min.hpp"
 #include <cmath>
-// #include"utils.hpp"
 
 double norm(const vec &v)
 {
-    // forse c'è da mettere dim come parametro e non richiamare n tutte
-    // le volte
     double norma = 0;
     for (size_t i = 0; i < v.size(); i++)
     {
@@ -20,7 +17,6 @@ vec minimizer::solve(F f, Fn df)
     vec x0 = params.start_point;
     // double f_val=params.f(x0);
     double f_val = f(x0);
-
     vec df_val = df(x0);
     // vec df_val=params.df(x0);
     vec x_new = x0;
@@ -30,7 +26,6 @@ vec minimizer::solve(F f, Fn df)
 
     for (size_t i = 0; i < params.max_it; i++)
     {
-
         for (size_t j = 0; j < params.dim; j++)
         {
             x_new[j] = x_old[j] - alpha * df_val[j];
@@ -40,11 +35,8 @@ vec minimizer::solve(F f, Fn df)
       
         if (std::abs(f(x_new) - f_val) < params.tolerance_r || norm(delta_x) < params.tolerance_s)
         {
-
             return x_new;
         }
-        // f_val=params.f(x_new);
-        // df_val=params.df(x_new);
         f_val = f(x_new);
         df_val = df(x_new);
         // alpha=search_alpha(x_new, f_val, df_val, alpha);
@@ -54,10 +46,8 @@ vec minimizer::solve(F f, Fn df)
     return x_new;
 }
 
-// double minimizer::search_alpha(const vec& x_k, const double f_val, const vec& df_val,const double alpha){
 double minimizer::search_alpha(const vec &x_k, const double f_val, const vec &df_val, const double alpha, F f)
 {
-
     double alpha_curr = alpha;
     // size_t dim=df_val.size();
     std::vector<double> f_diff = df_val;
@@ -66,7 +56,7 @@ double minimizer::search_alpha(const vec &x_k, const double f_val, const vec &df
         f_diff[i] = x_k[i] - alpha_curr * df_val[i];
     }
     double norm2 = norm(df_val);
-    // while ( !( f_val - params.f(f_diff)>= params.sigma*alpha_curr*norm2 ) )
+
     while (!(f_val - f(f_diff) >= params.sigma * alpha_curr * norm2))
     {
         alpha_curr *= 0.5;
@@ -75,6 +65,5 @@ double minimizer::search_alpha(const vec &x_k, const double f_val, const vec &df
             f_diff[i] = x_k[i] - alpha_curr * df_val[i];
         }
     }
-
     return alpha_curr;
 };
