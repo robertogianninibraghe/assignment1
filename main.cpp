c36222147659952d221ce00479176bbdb3e2ed51
robertogianninibraghe <roberto.giannini@mail.polimi.it>
2024-03-23 17:17:23 +0100

    Added the test file, and updated with new implementation multiv_min.hpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+//
+// Test file r
+//
+
+#include <iostream>
+
+#include "multiv_min.hpp"
+#include <cmath>
+
+
+using std::cout;
+
+double f_test(const vec &x)
+{
+    return x[0] * x[1] + 4 * std::pow(x[0], 4) + x[1] * x[1] + 3 * x[0];
+}
+
+vec df_test(const vec &x)
+{
+    vec grad = {x[1] + 16 * std::pow(x[0], 3) + 3, x[0] + 2 * x[1]};
+    return grad;
+}
+
+void print_vec(const vec &x){
+    for(const auto & i : x){
+        cout<< i << " "<<std::endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    parameters par;
+
+    minimizer min(par);
+    
+    vec result = min.solve(f_test, df_test);
+    cout<<"Old implementation of Armijo without choice of learning rate: "<<std::endl;
+    print_vec(result);
+
+    minimizer2<Exponential> solve_exp;
+    minimizer2<Inverse_decay> solve_inv;
+    minimizer2<Armijo> solve_arm;
+
+    vec res_exp = solve_exp(f_test, df_test);
+    vec res_inv = solve_inv(f_test, df_test);
+    vec res_arm = solve_arm(f_test, df_test);
+
+    cout << "Solution with exponential:"  << std::endl;
+    print_vec(res_exp);
+    cout << "Solution with inverse:" << std::endl;
+    print_vec(res_inv);
+    cout << "Solution with Armijo:"  << std::endl;
+    print_vec(res_arm);
+
+    return 0;
+}
--- multiv_min.hpp
+++ multiv_min.hpp
@@ -1,50 +1,152 @@
 #ifndef MULTIV_MIN_HPP
 #define MULTIV_MIN_HPP
 
-#include<iostream>
-#include<utils.hpp>
-#include<functional>
-/* #include<Eigen>*/
+#include <iostream>
+#include <functional>
+#include <cmath>
 
+using vec = std::vector<double>;
+using F = std::function<double(const vec &)>;
+using Fn = std::function<vec(const vec &)>;
 
+double norm(const vec &v);
 
-template<typename T>
-
-//using std::function<double(const std::vector<double> &)> as funz;
-
-/*struct parameters
+struct parameters
 {
-    double tolerance_r =10^(-6);
-    double tolerance_s =10^(-6);
-    T initial_cond;
-    double alpha_0 = 1;
-    double sigma=0.5;
-    unsigned max_it=10000;
-    std::function<double(const std::vector<double> &)> f;
-    std::function<std::vector<double>(const std::vector<double> &)> df;
-    T strategy;
-
+    double tolerance_r = std::pow(10, -6);
+    double tolerance_s = std::pow(10, -6);
+    vec start_point = {0, 0};
+    double alpha_0 = 0.1;
+    double mu = 0.2;
+    double sigma = 0.1;
+    unsigned max_it = 1000;
+    unsigned dim = 2;
 };
-template<typename T>
-*/
 
-class minimizer{
-    private:
+class minimizer
+{
+private:
     parameters params;
-    //double f_val=0;
-    //std::vector<double> df_val;
 
-    public:
-    minimizer(parameters parametri): params(parametri){};
-    vec solve(const vec &x0);
-    //da vedere quali metodi vanno const e quali no
-    double search_alpha(const vec& x_k, const double f_val, const vec& df_val,const double alpha);
-
+public:
+    minimizer(parameters parametri) : params(parametri){};
+    // vec solve(const vec& x0);
+    vec solve(F f, Fn df);
+    double search_alpha(const vec &x_k, const double f_val, const vec &df_val, const double alpha, F f);
 };
 
-//voglio usare i templates qui anzichè quello che ho usato
-//std::vector<double> multiv_min(std::function<double(double const &, double  const &)> const &f,
-//                  std::function<double(double const &, double  const &)> const &Df, T const & strategy, parameters param ){};
-// versione più generica
-//std::vector<T> min_2(std::function<)
+template <class lRate>
+class minimizer2
+{
+public:
+    vec operator()(F f, Fn df)
+    {
+        vec x0 = params.start_point;
+        // double f_val=params.f(x0);
+        double f_val = f(x0);
+        // std::cout<<"First f_val "<<f_val<<std::endl;
+        vec df_val = df(x0);
+        // vec df_val=params.df(x0);
+        vec x_new = x0;
+        vec x_old = x0;
+        double alpha = params.alpha_0;
+        vec delta_x = x0;
+        // need to check if dim written like this is visible
+        for (size_t i = 0; i < params.max_it; i++)
+        {
+            //std::cout << "Iterazione numero" << i << std::endl;
+            //used for debugging
+            for (size_t j = 0; j < params.dim; j++)
+            {
+                x_new[j] = x_old[j] - alpha * df_val[j];
+                delta_x[j] = x_new[j] - x_old[j];
+                x_old[j] = x_new[j];
+                // std::cout<< x_old[0]<<"  "<<x_old[1]<<std::endl;
+            }
+            // std::cout<<"f_val non dovrebbe cambiare"<<f_val<<std::endl;
+            // std::cout<<f(x_new)<<std::endl;
+
+            // std::cout<<"Modulo differenza: "<<std::abs(f(x_new)-f_val)<<std::endl;
+            // std::cout<<"Dstanza : "<< norm(delta_x)<<std::endl;
+            // if(abs(params.f(x_new)-f_val) < params.tolerance_r /*|| norm(delta_x)< params.tolerance_s*params.tolerance_s*/ ){
+            // std::cout<<params.tolerance_r<<": : "<<params.tolerance_s*params.tolerance_s<<std::endl;
+
+            if (std::abs(f(x_new) - f_val) < params.tolerance_r || norm(delta_x) < params.tolerance_s)
+            {
+                // std::cout<< "entro qui"<<std::endl;
+                return x_new;
+            }
+            f_val = f(x_new);
+            df_val = df(x_new);
+            // alpha=search_alpha(x_new, f_val, df_val, alpha);
+            alpha = search_alpha(x_new, f_val, df_val, params.alpha_0, f, i, params.mu, params.sigma);
+            
+        }
+        std::cout << "Method reched the maximum number of iterations" << std::endl;
+        return x_new;
+    }
+    parameters params;
+
+private:
+    lRate search_alpha;
+};
+
+class Exponential
+{
+public:
+    double operator()(const vec &x_k, const double f_val, const vec &df_val, const double alpha, F f, unsigned k, double mu, double sigma) const
+    {
+        //std::cout << "Chosing the learning rate by exponential decay " << std::endl;
+        //std::cout << "alpha with exp: " << alpha * std::exp(-1 * mu * k) << std::endl;
+        //commands i used to check if the methods were working properly
+
+        return alpha * std::exp(-1 * mu * k);
+    }
+};
+
+class Inverse_decay
+{
+public:
+    double operator()(const vec &x_k, const double f_val, const vec &df_val, const double alpha, F f, unsigned k, double mu, double sigma) const
+    {
+        //std::cout << "Chosing the learning rate by inverse decay " << std::endl;
+        //std::cout << "alpha with inverse " << alpha / (1 + mu * k) << std::endl;
+        //commands i used to check if the methods were working properly
+        return alpha / (1 + mu * k);
+    }
+};
+
+class Armijo
+{
+public:
+    double operator()(const vec &x_k, const double f_val, const vec &df_val, const double alpha, F f, unsigned k, double mu, double sigma) const
+    {
+
+        //std::cout << "Chosing the learning rate by Armijio " << std::endl;
+        //commands i used to check if the methods were working properly
+
+        double alpha_curr = alpha;
+        // size_t dim=df_val.size();
+        std::vector<double> f_diff = df_val;
+        unsigned dimen = df_val.size();
+        for (size_t i = 0; i < dimen; i++)
+        {
+            f_diff[i] = x_k[i] - alpha_curr * df_val[i];
+        }
+        double norm2 = norm(df_val);
+        while (!(f_val - f(f_diff) >= sigma * alpha_curr * norm2))
+        {
+            alpha_curr *= 0.5;
+            for (size_t i = 0; i < dimen; i++)
+            {
+                f_diff[i] = x_k[i] - alpha_curr * df_val[i];
+            }
+        }
+
+        return alpha_curr;
+    }
+};
+
 #endif
+
+
